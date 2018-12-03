1710385bfd297a9d1754b25caf5fe4cc835a6a46
Zayedkz <45078473+Zayedkz@users.noreply.github.com>
2018-12-03 16:58:32 -0500

    Joined location pages

--- js.js
+++ js.js
@@ -9,3 +9,17 @@ function calcTotal(element) {
 	document.getElementById("tax").innerHTML = tax.toFixed(2);
 	document.getElementById("total").innerHTML = total.toFixed(2);
 }
+
+function changeLocation(element) {
+	console.log(element.id);
+	document.getElementById("location1").style.display = "none";
+	document.getElementById("location2").style.display = "none";
+	document.getElementById("location3").style.display = "none";
+	if(element.id == 1) {
+		document.getElementById("location1").style.display = "block";
+	} else if(element.id == 2) {
+		document.getElementById("location2").style.display = "block";
+	} else {
+		document.getElementById("location3").style.display = "block";
+	}
+}
