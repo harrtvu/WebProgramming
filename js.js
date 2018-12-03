ed05a8cbd179d6041e8b442393359ef1a58ca9c2
Zayedkz <45078473+Zayedkz@users.noreply.github.com>
2018-12-02 19:27:30 -0500

    added booking page

--- js.js
+++ js.js
@@ -1,1 +1,11 @@
-
+function calcTotal(element) {
+	var price = element.value * 15.00;
+	console.log(price);
+	var tax = price * 0.13;
+	console.log(tax);
+	var total = price + tax;
+	console.log(total);
+	document.getElementById("price").innerHTML = price.toFixed(2);
+	document.getElementById("tax").innerHTML = tax.toFixed(2);
+	document.getElementById("total").innerHTML = total.toFixed(2);
+}
